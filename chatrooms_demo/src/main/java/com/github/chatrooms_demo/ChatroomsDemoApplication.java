3f93b1ef67c1b1ca1995e54f5087d5d3202fb4b0
Yzk258 <yzk24@mails.tsinghua.edu.cn>
2026-03-13 18:36:01 +0800

    提交我的 Java 项目，测试文件初始化

---
+++ chatrooms_demo/src/main/java/com/github/chatrooms_demo/ChatroomsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.chatrooms_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatroomsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatroomsDemoApplication.class, args);
+	}
+
+}
